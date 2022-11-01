750055232a7f039a19dee1cd2fa2a918e29d1862
Cloud Huang <y9740201@gmail.com>
2022-11-01 16:03:59 +0800

    change test cases

--- Lab2_RISC-V Toolchain/Origin code/BestTimeToBuyAndSellStock.c
+++ Lab2_RISC-V Toolchain/Origin code/BestTimeToBuyAndSellStock.c
@@ -14,12 +14,12 @@ int maxProfit(int *prices, int pricesSize)
 }
 
 int main(){
-	int prices_1[5] = {7,6,4,3,1};
-	int prices_2[6] = {7,1,5,3,6,4};
-	int prices_3[4] = {1,1,8,2};
-	int pricesSize_1 = 5;
-	int pricesSize_2 = 6;
-	int pricesSize_3 = 4;
+	int prices_1[6] = {7, 1, 5, 3, 6, 4};
+	int prices_2[4] = {1, 1, 3, 4};
+	int prices_3[5] = {7, 5, 4, 3, 2};
+	int pricesSize_1 = 6;
+	int pricesSize_2 = 4;
+	int pricesSize_3 = 5;
 	printf("%d\n", maxProfit(prices_1,pricesSize_1));
 	printf("%d\n", maxProfit(prices_2,pricesSize_2));
 	printf("%d\n", maxProfit(prices_3,pricesSize_3));
