d8162f9a6be42be2106ecd9084bc6788352a4cf1
maroma samsa <y9740201@gmail.com>
2022-12-21 20:46:36 +0000

    Feat: floorq and ceilq
    Rewrite `floorf()` and `ceilf()`.
    
    refer from Linux man page(3)
    ` - floorq`: return the largest integral value that is not greater than x
     - `ceilq`: return the smallest integral value that is not less than x

--- TermProject/line/qformat.h
+++ TermProject/line/qformat.h
@@ -40,9 +40,24 @@ static inline q_fmt q_mul(q_fmt a, q_fmt b){
     return (q_fmt)tmp >> Q;
 }
 
-/* division of Q format value, no support rounding for now*/
+/* division of Q format value, no support rounding for now */
 static inline q_fmt q_div(q_fmt a, q_fmt b){
     q_buf tmp = (q_buf)a << Q;
     return (q_fmt)(tmp / b);
+}
+
+/* return the largest integral value that is not greater than x */
+static inline q_fmt floorq(q_fmt x){
+    q_fmt mask = (0xFFFFFFFF >> Q) << Q;
+    q_fmt delta = x & ~mask;
+    return x & mask;
+}
+
+/* return the smallest integral value that is not less than x */
+static inline q_fmt ceilq(q_fmt x){
+    q_fmt mask = (0xFFFFFFFF >> Q) << Q;
+    q_fmt delta = x & ~mask;
+    x = x & mask;
+    return delta? q_add(x, 1<<Q): x;
 }
 
